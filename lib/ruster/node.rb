704926ea5cd3d9034e76385ef2afe2a8db939350
Leandro López (inkel) <inkel.ar@gmail.com>
2014-03-15 12:32:30 -0300

    Parse slots information
    Note that migrating/importing slots aren't being parsed at the moment.

--- lib/ruster/node.rb
+++ lib/ruster/node.rb
@@ -7,7 +7,6 @@ class Ruster::Node
   attr :pong_epoch
   attr :config_epoch
   attr :state
-  attr :slots
 
   def initialize(addr)
     @addr = addr
@@ -34,5 +33,18 @@ class Ruster::Node
     @config_epoch = parts.shift.to_i
     @state = parts.shift
     @slots = parts
+  end
+
+  def slots
+    @slots.map do |slots|
+      case slots
+      when /^(\d+)-(\d+)$/ then ($1.to_i..$2.to_i)
+      when /^\d+$/ then (slots.to_i..slots.to_i)
+      end
+    end
+  end
+
+  def all_slots
+    slots.map(&:to_a).flatten
   end
 end
--- test/node.rb
+++ test/node.rb
@@ -35,5 +35,47 @@ Protest.describe "Node" do
       assert_equal "connected", node.state
       assert_equal [], node.slots
     end
+
+    context "slots" do
+      test "contiguous block" do
+        info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 0-16384"
+
+        node = Ruster::Node.new("127.0.0.1:12701")
+
+        node.read_info_line!(info_line)
+
+        assert_equal [(0..16384)], node.slots
+      end
+
+      test "single" do
+        info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 4096"
+
+        node = Ruster::Node.new("127.0.0.1:12701")
+
+        node.read_info_line!(info_line)
+
+        assert_equal [(4096..4096)], node.slots
+      end
+
+      test "combined" do
+        info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 0-1024 2048 4096 8192-16383"
+
+        node = Ruster::Node.new("127.0.0.1:12701")
+
+        node.read_info_line!(info_line)
+
+        assert_equal [(0..1024), (2048..2048), (4096..4096), (8192..16383)], node.slots
+      end
+
+      test "#all_slots" do
+        info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 0-3 5 10-13 16383"
+
+        node = Ruster::Node.new("127.0.0.1:12701")
+
+        node.read_info_line!(info_line)
+
+        assert_equal [0, 1, 2, 3, 5, 10, 11, 12, 13, 16383], node.all_slots
+      end
+    end
   end
 end
