6c826141917d36c8287b6cfd27ed4480233edfed
Leandro López (inkel) <inkel.ar@gmail.com>
2014-03-15 12:17:38 -0300

    Read info line as sent by CLUSTER NODES

--- lib/ruster/node.rb
+++ lib/ruster/node.rb
@@ -1,5 +1,13 @@
 class Ruster::Node
   attr :addr
+  attr :id
+  attr :flags
+  attr :master_id
+  attr :ping_epoch
+  attr :pong_epoch
+  attr :config_epoch
+  attr :state
+  attr :slots
 
   def initialize(addr)
     @addr = addr
@@ -11,5 +19,20 @@ class Ruster::Node
 
   def enabled?
     client.call("INFO", "cluster").include?("cluster_enabled:1")
+  end
+
+  def read_info_line!(info_line)
+    parts = info_line.split
+
+    @id = parts.shift
+    addr = parts.shift
+    @flags = parts.shift.split(",")
+    @addr = addr unless @flags.include?("myself")
+    @master_id = parts.shift
+    @ping_epoch = parts.shift.to_i
+    @pong_epoch = parts.shift.to_i
+    @config_epoch = parts.shift.to_i
+    @state = parts.shift
+    @slots = parts
   end
 end
--- test/node.rb
+++ test/node.rb
@@ -16,4 +16,24 @@ Protest.describe "Node" do
       assert !node.enabled?
     end
   end
+
+  context "info" do
+    test "#read_info_line!" do
+      info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected"
+
+      node = Ruster::Node.new("127.0.0.1:12701")
+
+      node.read_info_line!(info_line)
+
+      assert_equal "9aee954a0b7d6b49d7e68c18d08873c56aaead6b", node.id
+      assert_equal "127.0.0.1:12701", node.addr
+      assert_equal ["myself", "master"], node.flags
+      assert_equal "-", node.master_id
+      assert_equal 0, node.ping_epoch
+      assert_equal 1, node.pong_epoch
+      assert_equal 2, node.config_epoch
+      assert_equal "connected", node.state
+      assert_equal [], node.slots
+    end
+  end
 end
