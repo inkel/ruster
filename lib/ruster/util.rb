33092c686b2cca4afe6e8dd23057b19be6d91d6c
Leandro López (inkel) <inkel.ar@gmail.com>
2014-03-15 13:51:04 -0300

    Ignore comments in INFO parsing

--- lib/ruster/util.rb
+++ lib/ruster/util.rb
@@ -1,7 +1,8 @@
 module Ruster::Util
   def parse_info(info)
     {}.tap do |data|
-      info.split.each do |line|
+      info.split("\r\n").each do |line|
+        next if line[0] == "#"
         key, val = line.split(":")
         data[key.to_sym] = val
       end
--- test/util.rb
+++ test/util.rb
@@ -1,10 +1,10 @@
 require_relative "./helper"
 
 Protest.describe "Ruster::Util" do
+  U = Module.new { extend Ruster::Util }
+
   test "parse INFO as hash"do
     info = "cluster_state:fail\r\ncluster_slots_assigned:0\r\ncluster_slots_ok:0\r\ncluster_slots_pfail:0\r\ncluster_slots_fail:0\r\ncluster_known_nodes:1\r\ncluster_size:0\r\ncluster_current_epoch:0\r\ncluster_stats_messages_sent:0\r\ncluster_stats_messages_received:0\r\n"
-
-    U = Module.new { extend Ruster::Util }
 
     data = U.parse_info(info)
 
@@ -20,5 +20,11 @@ Protest.describe "Ruster::Util" do
     assert_equal "0",    data[:cluster_current_epoch]
     assert_equal "0",    data[:cluster_stats_messages_sent]
     assert_equal "0",    data[:cluster_stats_messages_received]
+  end
+
+  test "ignore comments in INFO parsing" do
+    data = U.parse_info("# Cluster\r\ncluster_enabled:1\r\n")
+
+    assert_equal 1, data.size
   end
 end
