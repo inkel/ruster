95f9eb75dd796cf623f0e79a9ef15aac0549eddc
Leandro López (inkel) <inkel.ar@gmail.com>
2014-03-15 13:54:09 -0300

    Refactor Node tests

--- test/node.rb
+++ test/node.rb
@@ -17,101 +17,99 @@ Protest.describe "Node" do
     end
   end
 
-  context "info" do
-    test "#read_info_line!" do
-      info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected"
+  test "read and parses info line" do
+    info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected"
+
+    node = Ruster::Node.new("127.0.0.1:12701")
+
+    node.read_info_line!(info_line)
+
+    assert_equal "9aee954a0b7d6b49d7e68c18d08873c56aaead6b", node.id
+    assert_equal "127.0.0.1:12701", node.addr
+    assert_equal ["myself", "master"], node.flags
+    assert_equal "-", node.master_id
+    assert_equal 0, node.ping_epoch
+    assert_equal 1, node.pong_epoch
+    assert_equal 2, node.config_epoch
+    assert_equal "connected", node.state
+    assert_equal [], node.slots
+  end
+
+  context "allocated slots" do
+    test "contiguous block" do
+      info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 0-16383"
 
       node = Ruster::Node.new("127.0.0.1:12701")
 
       node.read_info_line!(info_line)
 
-      assert_equal "9aee954a0b7d6b49d7e68c18d08873c56aaead6b", node.id
-      assert_equal "127.0.0.1:12701", node.addr
-      assert_equal ["myself", "master"], node.flags
-      assert_equal "-", node.master_id
-      assert_equal 0, node.ping_epoch
-      assert_equal 1, node.pong_epoch
-      assert_equal 2, node.config_epoch
-      assert_equal "connected", node.state
-      assert_equal [], node.slots
+      assert_equal [(0..16383)], node.slots
+      assert       node.migrating.empty?
+      assert       node.importing.empty?
     end
 
-    context "slots" do
-      test "contiguous block" do
-        info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 0-16383"
+    test "single" do
+      info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 4096"
 
-        node = Ruster::Node.new("127.0.0.1:12701")
+      node = Ruster::Node.new("127.0.0.1:12701")
 
-        node.read_info_line!(info_line)
+      node.read_info_line!(info_line)
 
-        assert_equal [(0..16383)], node.slots
-        assert       node.migrating.empty?
-        assert       node.importing.empty?
-      end
+      assert_equal [(4096..4096)], node.slots
+      assert       node.migrating.empty?
+      assert       node.importing.empty?
+    end
 
-      test "single" do
-        info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 4096"
+    test "migrating" do
+      info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected [16383->-6daeaa65c37880d81c86e7d94b6d7b0a459eea9]"
 
-        node = Ruster::Node.new("127.0.0.1:12701")
+      node = Ruster::Node.new("127.0.0.1:12701")
 
-        node.read_info_line!(info_line)
+      node.read_info_line!(info_line)
 
-        assert_equal [(4096..4096)], node.slots
-        assert       node.migrating.empty?
-        assert       node.importing.empty?
-      end
+      assert_equal [], node.slots
+      assert_equal 1, node.migrating.size
+      assert_equal "6daeaa65c37880d81c86e7d94b6d7b0a459eea9", node.migrating[16383]
+      assert       node.importing.empty?
+    end
 
-      test "migrating" do
-        info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected [16383->-6daeaa65c37880d81c86e7d94b6d7b0a459eea9]"
+    test "importing" do
+      info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected [16383-<-6daeaa65c37880d81c86e7d94b6d7b0a459eea9]"
 
-        node = Ruster::Node.new("127.0.0.1:12701")
+      node = Ruster::Node.new("127.0.0.1:12701")
 
-        node.read_info_line!(info_line)
+      node.read_info_line!(info_line)
 
-        assert_equal [], node.slots
-        assert_equal 1, node.migrating.size
-        assert_equal "6daeaa65c37880d81c86e7d94b6d7b0a459eea9", node.migrating[16383]
-        assert       node.importing.empty?
-      end
+      assert_equal [], node.slots
+      assert_equal 1, node.importing.size
+      assert_equal "6daeaa65c37880d81c86e7d94b6d7b0a459eea9", node.importing[16383]
+      assert       node.migrating.empty?
+    end
 
-      test "importing" do
-        info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected [16383-<-6daeaa65c37880d81c86e7d94b6d7b0a459eea9]"
+    test "combined" do
+      info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 0-1024 2048 [3072->-6daeaa65c37880d81c86e7d94b6d7b0a459eea9] 4096 [6144-<-6daeaa65c37880d81c86e7d94b6d7b0a459eea9] 8192-16383"
 
-        node = Ruster::Node.new("127.0.0.1:12701")
+      node = Ruster::Node.new("127.0.0.1:12701")
 
-        node.read_info_line!(info_line)
+      node.read_info_line!(info_line)
 
-        assert_equal [], node.slots
-        assert_equal 1, node.importing.size
-        assert_equal "6daeaa65c37880d81c86e7d94b6d7b0a459eea9", node.importing[16383]
-        assert       node.migrating.empty?
-      end
+      assert_equal [(0..1024), (2048..2048), (4096..4096), (8192..16383)], node.slots
 
-      test "combined" do
-        info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 0-1024 2048 [3072->-6daeaa65c37880d81c86e7d94b6d7b0a459eea9] 4096 [6144-<-6daeaa65c37880d81c86e7d94b6d7b0a459eea9] 8192-16383"
+      assert_equal 1, node.migrating.size
+      assert_equal "6daeaa65c37880d81c86e7d94b6d7b0a459eea9", node.migrating[3072]
 
-        node = Ruster::Node.new("127.0.0.1:12701")
+      assert_equal 1, node.importing.size
+      assert_equal "6daeaa65c37880d81c86e7d94b6d7b0a459eea9", node.importing[6144]
+    end
 
-        node.read_info_line!(info_line)
+    test "all allocated slots as an array" do
+      info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 0-3 5 10-13 16383"
 
-        assert_equal [(0..1024), (2048..2048), (4096..4096), (8192..16383)], node.slots
+      node = Ruster::Node.new("127.0.0.1:12701")
 
-        assert_equal 1, node.migrating.size
-        assert_equal "6daeaa65c37880d81c86e7d94b6d7b0a459eea9", node.migrating[3072]
+      node.read_info_line!(info_line)
 
-        assert_equal 1, node.importing.size
-        assert_equal "6daeaa65c37880d81c86e7d94b6d7b0a459eea9", node.importing[6144]
-      end
-
-      test "#all_slots" do
-        info_line = "9aee954a0b7d6b49d7e68c18d08873c56aaead6b :0 myself,master - 0 1 2 connected 0-3 5 10-13 16383"
-
-        node = Ruster::Node.new("127.0.0.1:12701")
-
-        node.read_info_line!(info_line)
-
-        assert_equal [0, 1, 2, 3, 5, 10, 11, 12, 13, 16383], node.all_slots
-      end
+      assert_equal [0, 1, 2, 3, 5, 10, 11, 12, 13, 16383], node.all_slots
     end
   end
 end
