e94efc709c6dee06839f7d92640861c0f29250b6
Leandro López (inkel) <inkel.ar@gmail.com>
2014-03-25 14:33:24 -0300

    Remove unnecessary methods
    Thanks @badboy!

--- lib/ruster/node.rb
+++ lib/ruster/node.rb
@@ -48,7 +48,7 @@ class Ruster::Node
     @migrating = {}
     @importing = {}
 
-    parts.map do |slots|
+    parts.each do |slots|
       case slots
       when /^(\d+)-(\d+)$/ then @slots << ($1.to_i..$2.to_i)
       when /^\d+$/ then @slots << (slots.to_i..slots.to_i)
@@ -58,7 +58,7 @@ class Ruster::Node
         when "<" then @importing[$1.to_i] = $3
         end
       end
-    end.compact
+    end
   end
 
   def all_slots
